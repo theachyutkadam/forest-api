60367d2ad4179b1673caf21ccddb64c714c1c966
achyut kadam <achyut.k@acldigital.com>
2022-10-31 16:34:13 +0530

    Initial API setup

---
+++ app/controllers/forests_controller.rb
@@ -0,0 +1,28 @@
+class ForestsController < ApplicationController
+	def index
+		@forests = Forest.all
+		render json: @forests
+	end
+
+	def show
+		@forest = Forest.find(params[:id])
+		render ison: @forest
+	end
+
+	def create
+		@forest = Forest.create(name: params[:name], state: params[:state])
+		render json: @forest
+	end
+
+	def update
+		@forest = Forest.find(params[:id])
+		@forest.update(name: params[:name], state: params[:state])
+		render json: "#{@forest.name} has been updated!"
+	end
+
+	def destroy
+		@forest = Forest.find(params[:id])
+		@forest.destroy
+		render json: "#{@forest.name} has been deleted!"
+	end
+end
